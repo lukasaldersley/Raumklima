b187376491cc0d9ecf7fa9c162f3e12ab9f35e12
Lukas Aldersley <aldersleluk@hotmail.com>
2017-05-31 23:53:37 +0200

    Arduino, Java und UWP (fokus auf UWP) Update

--- UWP/Raumklima/Raumklima/MainPage.xaml.cs
+++ UWP/Raumklima/Raumklima/MainPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +25,49 @@ namespace Raumklima
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private TextBox[] box;
+        private DeviceInformation[] availableDevices;
+
         public MainPage()
         {
             this.InitializeComponent();
+            picker.SelectionChanged += DeviceSelected;
+        }
+
+        private void DeviceSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (picker.SelectedIndex == 0)
+            {
+                //nix
+            }
+            else
+            {
+                info.Text = availableDevices[picker.SelectedIndex-1].Id + "\n" + availableDevices[picker.SelectedIndex-1].IsDefault + "\n" + availableDevices[picker.SelectedIndex-1].IsEnabled + "\n" + availableDevices[picker.SelectedIndex-1].Name + "\n" + availableDevices[picker.SelectedIndex-1].Pairing + "\n" + availableDevices[picker.SelectedIndex-1].Properties + "\n" + availableDevices[picker.SelectedIndex-1].EnclosureLocation;
+            }
+        }
+
+        private async void FindDevicesButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selector = BluetoothDevice.GetDeviceSelector();
+            var devices = await DeviceInformation.FindAllAsync(selector);
+            box = new TextBox[devices.Count];
+            availableDevices =devices.ToArray();
+            for (int i = 0; i < availableDevices.Length; i++)
+            {
+                box[i] = new TextBox();
+                box[i].Text = availableDevices[i].Name;
+                box[i].IsReadOnly = true;
+                box[i].BorderThickness = new Thickness(0);
+                box[i].IsHitTestVisible = false;
+                //box[i].FontSize = 20;
+
+                picker.Items.Add(box[i]);
+            }
+        }
+
+        private void ConnectToDeviceButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
