31bb07613857b3338ca5c7eb1b12ea33cde5a72c
Lukas Aldersley <aldersleluk@hotmail.com>
2017-06-01 23:14:05 +0200

    UWP und Arduino

--- UWP/Raumklima/Raumklima/MainPage.xaml.cs
+++ UWP/Raumklima/Raumklima/MainPage.xaml.cs
@@ -38,91 +38,67 @@ namespace Raumklima
             //DataReader reader = new DataReader(_socket.InputStream);
         }
 
-        private async void ConnectToDeviceButton_Click(object sender,RoutedEventArgs e)
+        private async void ConnectToDeviceButton_Click(object sender, RoutedEventArgs e)
         {
             info.Text = string.Empty;
-               try
-               {
-                   var devices =
-                   await DeviceInformation.FindAllAsync(
-                   RfcommDeviceService.GetDeviceSelector(
-                   RfcommServiceId.SerialPort));
+            try
+            {
+                var devices =
+                await DeviceInformation.FindAllAsync(
+                RfcommDeviceService.GetDeviceSelector(
+                RfcommServiceId.SerialPort));
 
                 DeviceInformation[] dev = devices.ToArray();
-                for(int i = 0; i < dev.Length; i++)
+                for (int i = 0; i < dev.Length; i++)
                 {
                     info.Text += dev[i].Name + "\n";
                 }
 
+                var device = devices.Single(x => x.Name == "Dev B");
+                _service = await RfcommDeviceService.FromIdAsync(
+                device.Id);
 
-                   var device = devices.Single(x => x.Name == "Dev B");
+                _socket = new StreamSocket();
 
-                   _service = await RfcommDeviceService.FromIdAsync(
-                   device.Id);
+                await _socket.ConnectAsync(
+                _service.ConnectionHostName,
+                _service.ConnectionServiceName,
+                SocketProtectionLevel.
+                BluetoothEncryptionAllowNullAuthentication);
 
-                   _socket = new StreamSocket();
-
-                    await _socket.ConnectAsync(
-                    _service.ConnectionHostName,
-                    _service.ConnectionServiceName,
-                    SocketProtectionLevel.
-                    BluetoothEncryptionAllowNullAuthentication);
-
-
-
-
-                int threadId;
-
-                // Create the delegate.
-                AsyncMethodCaller caller = new AsyncMethodCaller(recieceData);
-
-                // Initiate the asychronous call.
-                IAsyncResult result = caller.BeginInvoke(out threadId,null,null);
-
-
-
-
+                DataReader reader = new DataReader(_socket.InputStream);
+                while (true)
+                {
+                    info.Text += reader.ReadByte();
+                    //info.Text += reader.ReadString(140);//maximal 113(glaub ich aber zur sicherkeit mehr); wird zum schluss mit XXXXXX aufgefüllt
+                }
             }
             catch (Exception ex)
-                {
-                    info.Text += ex.Message+"\n";
+            {
+                info.Text += ex.Message + "\n";
                 info.Text += ex.StackTrace + "\n";
                 info.Text += ex.Source + "\n";
-                }
-        }
-
-
-        
-    // The delegate must have the same signature as the method
-    // it will call asynchronously.
-    public delegate void AsyncMethodCaller(out int threadId);
-
-    private void recieceData(out int threadId)
-        {
-            DataReader reader = new DataReader(_socket.InputStream);
-            while (true)
-            {
-                info.Text += reader.ReadString(140);//maximal 113(glaub ich aber zur sicherkeit mehr); wird zum schluss mit XXXXXX aufgefüllt
             }
         }
 
-            private async void btnDisconnect_Click(object sender,
-            RoutedEventArgs e)
-            {
-                info.Text = string.Empty;
 
-                try
-                {
-                    await _socket.CancelIOAsync();
-                    _socket.Dispose();
-                    _socket = null;
-                    _service.Dispose();
-                    _service = null;
-                }
-                catch (Exception ex)
-                {
-                    info.Text = ex.Message;
-                }
+        private async void btnDisconnect_Click(object sender,
+        RoutedEventArgs e)
+        {
+            info.Text = string.Empty;
+
+            try
+            {
+                await _socket.CancelIOAsync();
+                _socket.Dispose();
+                _socket = null;
+                _service.Dispose();
+                _service = null;
+            }
+            catch (Exception ex)
+            {
+                info.Text = ex.Message;
             }
         }
     }
+}
