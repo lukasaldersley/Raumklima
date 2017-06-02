42c00c630c12ac42edd748cfde0a80ed7f27943d
Lukas Aldersley <aldersleluk@hotmail.com>
2017-06-02 19:36:29 +0200

    arduino gui nearly working

--- Arduino/Raumklima/Raumklima.ino
+++ Arduino/Raumklima/Raumklima.ino
@@ -40,20 +40,21 @@ const int BRIGHTNESS_PIN = A2;
 const int LOUDNESS_PIN = A3;
 const int DHT_PIN = 4;
 
-const int DIRECT_LCD_RS_PIN=49;
-const int DIRECT_LCD_E_PIN=47;
-const int DIRECT_LCD_CONTRAST_PIN=45;
-const int DIRECT_LCD_BACKLIGHT_PIN=44;
-const int DIRECT_LCD_D4_PIN=41;
-const int DIRECT_LCD_D5_PIN=39;
-const int DIRECT_LCD_D6_PIN=37;
-const int DIRECT_LCD_D7_PIN=35;
+const int DIRECT_LCD_RS_PIN = 49;
+const int DIRECT_LCD_E_PIN = 47;
+const int DIRECT_LCD_CONTRAST_PIN = 45;
+const int DIRECT_LCD_BACKLIGHT_PIN = 44;
+const int DIRECT_LCD_D4_PIN = 41;
+const int DIRECT_LCD_D5_PIN = 39;
+const int DIRECT_LCD_D6_PIN = 37;
+const int DIRECT_LCD_D7_PIN = 35;
 
 //GENERAL SYSTEM VARS--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-unsigned long datasetCounter = 0;//?
+boolean directLcdEnabled = true;
 
 long baudrate = 115200;
-long displayCounter;
+int changeScreenThreshold = 10;
+int displayCounter = 0;
 int BMPReadingDelay = 0;
 
 String sendoff = "";
@@ -65,7 +66,7 @@ File file;
 DHT dht(DHT_PIN, DHTTYPE);
 SFE_BMP180 BMP180;
 Adafruit_BME280 BME280;
-LiquidCrystal directLcd(DIRECT_LCD_RS_PIN,DIRECT_LCD_E_PIN,DIRECT_LCD_D4_PIN,DIRECT_LCD_D5_PIN,DIRECT_LCD_D6_PIN,DIRECT_LCD_D7_PIN);
+LiquidCrystal directLcd(DIRECT_LCD_RS_PIN, DIRECT_LCD_E_PIN, DIRECT_LCD_D4_PIN, DIRECT_LCD_D5_PIN, DIRECT_LCD_D6_PIN, DIRECT_LCD_D7_PIN);
 
 
 //DATA VARS------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -81,7 +82,7 @@ double BMP180_Temperature = 0.0;
 double BME280_Airpressure = 0.0;
 double BME280_Temperature = 0.0;
 double BME280_Humidity = 0.0;
-double RTC_Temperature=0.0;
+double RTC_Temperature = 0.0;
 double Loudness = 0.0;
 double Brightness = 0.0;
 
@@ -94,18 +95,18 @@ void setup() {
   Serial.begin(115200);
   Serial.println("INITING");
 
-  analogWrite(DIRECT_LCD_CONTRAST_PIN,128);
-  analogWrite(DIRECT_LCD_BACKLIGHT_PIN,255);
+  analogWrite(DIRECT_LCD_CONTRAST_PIN, 128);
+  analogWrite(DIRECT_LCD_BACKLIGHT_PIN, 255);
 
-  directLcd.begin(20,4);
+  directLcd.begin(20, 4);
   directLcd.clear();
   directLcd.print("Initialisieren...");
 
   pinMode(3, INPUT_PULLUP); //ich hab den widerstand auf dem board vergessen deswegen der interne
-  attachInterrupt(1, alwaysInterruptButton_Push, FALLING);
+  attachInterrupt(1, alwaysInterruptButton_Push, LOW);
 
-    directLcd.setCursor(0,1);
-    directLcd.print("SD-Karte: ");
+  directLcd.setCursor(0, 1);
+  directLcd.print("SD-Karte: ");
   if (!SD.begin(SD_PIN)) { //readWrite Sample   //INITIALIZE SD-CARD
     Serial.println("SD-FAIL");
     directLcd.print("FAIL");
@@ -117,17 +118,17 @@ void setup() {
 
   fileName = getTimeName();
   Serial.println(fileName);
-  directLcd.setCursor(0,2);
+  directLcd.setCursor(0, 2);
   directLcd.print("DATEINAME fuer CSV:");
-  directLcd.setCursor(0,3);
+  directLcd.setCursor(0, 3);
   directLcd.print(fileName);
 
   Serial.println("BME280_Temperature;BME280_Humidity;BME280_Airpressure;BMP180_Temperature;BMP180_Airpressure;DHT_Temperature;DHT_HEAT_INDEX;DHT_Humidity;RTC_Temperature;TOTAL_Temperature;TOTAL_Airpressure;TOTAL_Humidity;Brightness;Loudness");
 
-delay(500);
-directLcd.clear();
-directLcd.print("SD-Schreibzugriff:");
-directLcd.setCursor(0,1);
+  delay(500);
+  directLcd.clear();
+  directLcd.print("SD-Schreibzugriff:");
+  directLcd.setCursor(0, 1);
   file = SD.open(fileName, FILE_WRITE);
   if (file) {
     file.println("BME280_Temperature;BME280_Humidity;BME280_Airpressure;BMP180_Temperature;BMP180_Airpressure;DHT_Temperature;DHT_HEAT_INDEX;DHT_Humidity;RTC_Temperature;TOTAL_Temperature;TOTAL_Airpressure;TOTAL_Humidity;Brightness;Loudness");
@@ -148,22 +149,24 @@ directLcd.setCursor(0,1);
     }
   }
 
-directLcd.setCursor(0,2);
-directLcd.print("DHT-Sensor: ");
+  directLcd.setCursor(0, 2);
+  directLcd.print("DHT-Sensor: ");
   dht.begin();
   directLcd.print("OK");
-  directLcd.setCursor(0,3);
+  directLcd.setCursor(0, 3);
   directLcd.print("BMP180/BME280: ");
   BMP180.begin();
   BME280.begin();
   directLcd.print("OK");
   delay(500);
   directLcd.clear();
-  directLcd.print("FERTIG");
-  directLcd.setCursor(0,2);
+  directLcd.print("FERTIG!");
+  directLcd.setCursor(0, 2);
   directLcd.print("DATEINAME fuer CSV:");
-  directLcd.setCursor(0,3);
+  directLcd.setCursor(0, 3);
   directLcd.print(fileName);
+  delay(2000);
+  directLcd.clear();
 }
 
 //GET SENSOR DATA-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -179,8 +182,8 @@ void getSensorData() {
   getPressureValues();
 }
 
-void getRTCValues(){
-  RTC_Temperature=RTC.temperature()/4.0;
+void getRTCValues() {
+  RTC_Temperature = RTC.temperature() / 4.0;
 }
 
 void getBMEValues() {
@@ -232,7 +235,7 @@ void getPressureValues() {
 }
 
 void getTemperatureValues() {
-  TOTAL_Temperature = (DHT_Temperature + BMP180_Temperature + BME280_Temperature+RTC_Temperature) / 4;
+  TOTAL_Temperature = (DHT_Temperature + BMP180_Temperature + BME280_Temperature + RTC_Temperature) / 4;
 }
 
 void getHumidityValues() {
@@ -312,13 +315,13 @@ void printDataToUART0() { //Main Serial Port
 }
 
 void printDataToUART2() { //BT-Module
-  int x=((int)(sendoff.length()));
-  Serial.println(x);
-  for(int i=x;i<140;i++){//sollten 140 zeichen sein
-    sendoff+="X";
+  int x = ((int)(sendoff.length()));
+  //Serial.println(x);
+  for (int i = x; i < 140; i++) { //sollten 140 zeichen sein
+    sendoff += "X";
   }
-  x=((int)(sendoff.length()));
-  Serial.println(x);
+  x = ((int)(sendoff.length()));
+  //Serial.println(x);
   Serial2.print(sendoff);
 }
 
@@ -331,6 +334,109 @@ void loop() {
   printDataToUART0();
   printDataToUART2();
   delay(1000);
+  Serial.println(displayCounter);
+  if (displayCounter < changeScreenThreshold) { //DHT22
+    displayCounter++;
+    directLcd.clear();
+    directLcd.print("DHT22-Werte");
+    directLcd.setCursor(0, 1);
+    directLcd.print("Temp: ");
+    directLcd.print(DHT_Temperature);
+    directLcd.setCursor(0, 2);
+    directLcd.print("HIC: ");
+    directLcd.print(DHT_Heat_Index);
+    directLcd.setCursor(0, 3);
+    directLcd.print("Hum: ");
+    directLcd.print(DHT_Humidity);
+  }
+  else if (displayCounter < changeScreenThreshold * 2) { //BMP180
+    displayCounter++;
+    directLcd.clear();
+    directLcd.print("BMP180-Werte");
+    directLcd.setCursor(0, 1);
+    directLcd.print("Temp: ");
+    directLcd.print(BMP180_Temperature);
+    directLcd.setCursor(0, 2);
+    directLcd.print("Luftdruck: ");
+    directLcd.print(BMP180_Airpressure);
+  }
+  else if (displayCounter < changeScreenThreshold * 3) { //BME280
+    displayCounter++;
+    directLcd.clear();
+    directLcd.print("BME280-Werte");
+    directLcd.setCursor(0, 1);
+    directLcd.print("Temp: ");
+    directLcd.print(BME280_Temperature);
+    directLcd.setCursor(0, 2);
+    directLcd.print("Luftdruck: ");
+    directLcd.print(BME280_Airpressure);
+    directLcd.setCursor(0, 3);
+    directLcd.print("Hum: ");
+    directLcd.print(BME280_Humidity);
+  }
+  else if (displayCounter < changeScreenThreshold * 4) { //RTC,LOUDNESS,BRIGHTNESS
+    displayCounter++;
+    directLcd.clear();
+    directLcd.print("SONSTIGES");
+    directLcd.setCursor(0, 1);
+    directLcd.print("RTC-Temp: ");
+    directLcd.print(RTC_Temperature);
+    directLcd.setCursor(0, 2);
+    directLcd.print("Helligkeit: ");
+    directLcd.print(Brightness);
+    directLcd.setCursor(0, 3);
+    directLcd.print("Lautstaerke: ");
+    directLcd.print(Loudness);
+  }
+  else if (displayCounter < changeScreenThreshold * 5) { //temperaturen
+    displayCounter++;
+    directLcd.clear();
+    directLcd.print("TEMPERATUREN");
+    directLcd.setCursor(0, 1);
+    directLcd.print("BMP/E: ");
+    directLcd.print(BMP180_Temperature);
+    directLcd.print("/");
+    directLcd.print(BME280_Temperature);
+    directLcd.setCursor(0, 2);
+    directLcd.print("RTC/DHT22: ");
+    directLcd.print(RTC_Temperature);
+    directLcd.print("/");
+    directLcd.print(DHT_Temperature);
+    directLcd.setCursor(0, 3);
+    directLcd.print("DURCHSCHNITT: ");
+    directLcd.print(TOTAL_Temperature);
+  }
+  else if (displayCounter < changeScreenThreshold * 6) { //luftfeuchtigkeiten
+    displayCounter++;
+    directLcd.clear();
+    directLcd.print("LUFTFEUCHTIGKEITEN");
+    directLcd.setCursor(0, 1);
+    directLcd.print("BME280 ");
+    directLcd.print(BME280_Humidity);
+    directLcd.setCursor(0, 2);
+    directLcd.print("DHT22: ");
+    directLcd.print(DHT_Humidity);
+    directLcd.setCursor(0, 3);
+    directLcd.print("DURCHSCHNITT: ");
+    directLcd.print(TOTAL_Humidity);
+  }
+  else if (displayCounter < changeScreenThreshold * 7) { //luftdrücke
+    displayCounter++;
+    directLcd.clear();
+    directLcd.print("LUFTDRUECKE");
+    directLcd.setCursor(0, 1);
+    directLcd.print("BMP180 ");
+    directLcd.print(BMP180_Airpressure);
+    directLcd.setCursor(0, 2);
+    directLcd.print("BME280: ");
+    directLcd.print(BME280_Airpressure);
+    directLcd.setCursor(0, 3);
+    directLcd.print("DURCHSCHNITT: ");
+    directLcd.print(TOTAL_Airpressure);
+  }
+  else {
+    displayCounter = 0;
+  }
 }
 
 String getTimeName() {
@@ -363,6 +469,16 @@ String getTimeName() {
 }
 
 void alwaysInterruptButton_Push() {
-  ;
+  if (directLcdEnabled) {
+    analogWrite(DIRECT_LCD_BACKLIGHT_PIN, 0);
+    analogWrite(DIRECT_LCD_CONTRAST_PIN, 255);
+    directLcdEnabled=false;
+  }
+  else {
+    analogWrite(DIRECT_LCD_CONTRAST_PIN, 128);
+    analogWrite(DIRECT_LCD_BACKLIGHT_PIN, 255);
+    directLcdEnabled=true;
+  }
+  delay(500);
 }
 
